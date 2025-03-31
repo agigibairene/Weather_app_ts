e158d25b30ba52f758c5023992f809ef50103714
agigibairene <i.agigiba@alustudent.com>
2025-03-31 10:23:28 +0200

    home page

---
+++ src/assets/api.ts
@@ -0,0 +1,34 @@
+export const weatherData = [
+    {
+        "lat": "51.50853N",
+        "lon": "0.12574W",
+        "elevation": 25,
+        "timezone": "Europe/London",
+        "units": "uk",
+        "current": {
+          "icon": "sunny",
+          "icon_num": 2,
+          "summary": "Sunny",
+          "temperature": 4.8,
+          "feels_like": 4,
+          "wind_chill": 4,
+          "dew_point": 2,
+          "wind": {
+            "speed": 3.1,
+            "gusts": 4.5,
+            "angle": 46,
+            "dir": "NE"
+          },
+          "precipitation": {
+            "total": 0,
+            "type": "none"
+          },
+          "cloud_cover": 0,
+          "ozone": 278.03,
+          "pressure": 1028,
+          "uv_index": 0.41,
+          "humidity": 87,
+          "visibility": 11.05
+        }
+      }
+]
--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -1,33 +1,95 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import {weatherData } from "../assets/api";
 
 export default function CurrentWeather(){
-    const [weatherData, setWeatherData] = useState("")
+    // const [weatherInfo, setWeatherData] = useState(weatherData)
 
-    const url = 'https://ai-weather-by-meteosource.p.rapidapi.com/current?lat=37.81021&lon=-122.42282&timezone=auto&language=en&units=auto';
-    const options = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-key': import.meta.env.VITE_weather_apiKey,
-            'x-rapidapi-host': 'ai-weather-by-meteosource.p.rapidapi.com'
-        }
-    };
+    // const url = 'https://ai-weather-by-meteosource.p.rapidapi.com/current?lat=37.81021&lon=-122.42282&timezone=auto&language=en&units=auto';
+    // const options = {
+    //     method: 'GET',
+    //     headers: {
+    //         'x-rapidapi-key': import.meta.env.VITE_weather_apiKey,
+    //         'x-rapidapi-host': 'ai-weather-by-meteosource.p.rapidapi.com'
+    //     }
+    // };
 
-    const fetchWeatherData = async () =>{
-        const response =  await fetch(url, options)
-        const data = await response.json()
+    // const fetchWeatherData = async () =>{
+    //     const response =  await fetch(url, options)
+    //     const data = await response.json()
 
-        setWeatherData(data)
+    //     setWeatherData(data)
+    // }
+
+    // useEffect(()=>{
+    //     fetchWeatherData()
+    // }, [])
+    interface WeatherProps{
+        icon?: string; 
+        value:string,
+        title: string
     }
 
-    useEffect(()=>{
-        fetchWeatherData()
-    }, [])
+    const weather: WeatherProps[] = [
+        {
+            icon: "bi bi-droplet mr-4",
+            value: `${weatherData[0]["current"]["precipitation"]["total"].toString()} mmh`,
+            title: "Precipation"
+        },
+        {
+            icon: "bi bi-moisture mr-4",
+            value: `${weatherData[0]["current"]["humidity"].toString()} %`,
+            title: "Humidity"
+        },
+        {
+            icon: "bi bi-clouds-fill mr-4",
+            value: `${weatherData[0]["current"]["cloud_cover"].toString()} %`,
+            title: "Clouds Cover"
+        },
+        {
+            icon: "bi bi-wind mr-4",
+            value: `${weatherData[0]["current"]["wind"]["speed"].toString()} %`,
+            title: "Wind"
+        },
+        {
+            icon: "bi bi-moisture mr-4",
+            value: `${weatherData[0]["current"]["uv_index"].toString()}`,
+            title: "UV index"
+        },
+        {
+            icon: "bi bi-eye mr-4",
+            value: `${weatherData[0]["current"]["uv_index"].toString()} mi`,
+            title: "Visibility"
+        },
+    ]
 
-    console.log(weatherData)
+
 
     return(
-        <div>
-            <h1>I am the  CurrentWeather</h1>
+        <div className="flex  gap-4 m-10">
+            <div className="overcast px-9 py-8 rounded-lg w-3/12">
+                <img src={`/public/set01/big/${weatherData[0]["current"]["icon_num"]}.png`} alt="" />
+                <p className="temperature text-4xl font-bold">{weatherData[0]["current"]['temperature']} °C</p>
+                <p className="feels-like my-4">feels like {weatherData[0]["current"]['feels_like']} °C</p>
+                <p className="text-3xl">Overcast</p>
+            </div>
+
+            <div className="details rounded-lg p-10  px-16 grid grid-cols-2 gap-x-40 gap-y-10 max-w-max">
+                {/* <div className="flex flex-col gap-10"> */}
+                  {
+                    weather.map((item : WeatherProps)=> {
+                        const {title, icon, value} = item;
+                        return <div>
+                         <p>
+                            <i className={icon}></i>
+                                {value}
+                            <span className="block text-center">{title}</span>
+                        </p>
+                        </div>
+                    })
+                  }
+                {/* </div> */}
+
+            </div>
         </div>
     )
 }
